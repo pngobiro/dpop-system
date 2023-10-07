35457b871ddf434a2ac4a03a28f2ca1b82d3ac94
Patrick Ngobiro <pngobiro@gmail.com>
2023-10-07 23:22:39 +0300

    updated

--- apps/statistics/urls.py
+++ apps/statistics/urls.py
@@ -14,16 +14,39 @@ urlpatterns = [
 
     path('', views.home, name='home'),
 
-    path('rank/<int:id>/<int:financial_year_id>/<int:financial_quarter_id>', views.rank_units, name='rank_units'),
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units', views.rank_units, name='rank_units'),
+
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id/months', views.rank_unit_division_months, name='unit_division_months'),
+    
+    path('rank/<int:id>/<int:financial_year_id>/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id>/months/<int:month_id>/cases/dcrt_summary', views.rank_unit_division_month_cases_summary, name='unit_division_month_cases_summary'),
+
+    # upload unit_monthly_dcrt_excel
+
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id>/months/<int:month_id>/cases/upload_dcrt', views.upload_unit_monthly_dcrt_excel, name='upload_unit_monthly_dcrt_excel'),
 
 
+    # save excel data to db
+
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id>/months/<int:month_id>/cases/save_dcrt', views.save_unit_monthly_dcrt_excel, name='save_unit_monthly_dcrt_excel'),
 
 
-    # path('rank/<int:id>/<int:financial_year_id>/<int:financial_quarter_id>/units/<int:unit_id>/months', views.rank_unit_months, name='rank_unit_months'),
+    # remove missing values from dataset
+
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id>/months/<int:month_id>/cases/remove_missing_values', views.remove_missing_values, name='remove_missing_values'),
+
+    # remove outliers from dataset
+
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id>/months/<int:month_id>/cases/remove_outliers', views.remove_outliers, name='remove_outliers'),
+
+    # view missing values
+
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id>/months/<int:month_id>/cases/view_missing_values', views.view_missing_values, name='view_missing_values'),
+
+    # view outliers
+
+    path('rank/<int:id>/fy/<int:financial_year_id>/fq/<int:financial_quarter_id>/units/<int:unit_id>/division/<int:division_id>/months/<int:month_id>/cases/view_outliers', views.view_outliers, name='view_outliers'),
 
 
-
-    # path('rank/<int:id>/<int:financial_year_id>/<int:financial_quarter_id>/units/<int:unit_id>/months/<int:month_id>', views.rank_unit_month, name='rank_unit_month'),
-
+    
     
 ]
--- apps/statistics/views.py
+++ apps/statistics/views.py
@@ -23,7 +23,7 @@ def rank_units(request, id, financial_year_id, financial_quarter_id):
     }
     return render(request, 'statistics/rank_units.html', context)
 
-def rank_unit_months(request, id, financial_year_id, financial_quarter_id, unit_id):
+def rank_unit_division_months(request, id, financial_year_id, financial_quarter_id, unit_id):
     # Handle logic for rank unit months view here
     context = {
         'id': id,
@@ -43,3 +43,93 @@ def rank_unit_month(request, id, financial_year_id, financial_quarter_id, unit_i
         'month_id': month_id,
     }
     return render(request, 'statistics/rank_unit_month.html', context)
+
+def rank_unit_division_month_cases_summary(request, id, financial_year_id, financial_quarter_id, unit_id, division_id, month_id):
+    # Handle logic for rank unit division month cases summary view here
+    context = {
+        'id': id,
+        'financial_year_id': financial_year_id,
+        'financial_quarter_id': financial_quarter_id,
+        'unit_id': unit_id,
+        'division_id': division_id,
+        'month_id': month_id,
+    }
+    return render(request, 'statistics/rank_unit_division_month_cases_summary.html', context)
+
+def upload_unit_monthly_dcrt_excel(request, id, financial_year_id, financial_quarter_id, unit_id, division_id, month_id):
+    # Handle logic for upload unit monthly dcrt excel view here
+    context = {
+        'id': id,
+        'financial_year_id': financial_year_id,
+        'financial_quarter_id': financial_quarter_id,
+        'unit_id': unit_id,
+        'division_id': division_id,
+        'month_id': month_id,
+    }
+    return render(request, 'statistics/upload_unit_monthly_dcrt_excel.html', context)
+
+def save_unit_monthly_dcrt_excel(request, id, financial_year_id, financial_quarter_id, unit_id, division_id, month_id):
+    # Handle logic for save unit monthly dcrt excel view here
+    context = {
+        'id': id,
+        'financial_year_id': financial_year_id,
+        'financial_quarter_id': financial_quarter_id,
+        'unit_id': unit_id,
+        'division_id': division_id,
+        'month_id': month_id,
+    }
+    return render(request, 'statistics/save_unit_monthly_dcrt_excel.html', context)
+
+
+def remove_missing_values(request, id, financial_year_id, financial_quarter_id, unit_id, division_id, month_id):
+    # Handle logic for remove missing values view here
+    context = {
+        'id': id,
+        'financial_year_id': financial_year_id,
+        'financial_quarter_id': financial_quarter_id,
+        'unit_id': unit_id,
+        'division_id': division_id,
+        'month_id': month_id,
+    }
+    return render(request, 'statistics/remove_missing_values.html', context)
+
+
+def remove_outliers(request, id, financial_year_id, financial_quarter_id, unit_id, division_id, month_id):
+    # Handle logic for remove outliers view here
+    context = {
+        'id': id,
+        'financial_year_id': financial_year_id,
+        'financial_quarter_id': financial_quarter_id,
+        'unit_id': unit_id,
+        'division_id': division_id,
+        'month_id': month_id,
+    }
+    return render(request, 'statistics/remove_outliers.html', context)
+
+
+def view_missing_values(request, id, financial_year_id, financial_quarter_id, unit_id, division_id, month_id):
+    # Handle logic for view missing values view here
+    context = {
+        'id': id,
+        'financial_year_id': financial_year_id,
+        'financial_quarter_id': financial_quarter_id,
+        'unit_id': unit_id,
+        'division_id': division_id,
+        'month_id': month_id,
+    }
+    return render(request, 'statistics/view_missing_values.html', context)
+
+
+def view_outliers(request, id, financial_year_id, financial_quarter_id, unit_id, division_id, month_id):
+    # Handle logic for view outliers view here
+    context = {
+        'id': id,
+        'financial_year_id': financial_year_id,
+        'financial_quarter_id': financial_quarter_id,
+        'unit_id': unit_id,
+        'division_id': division_id,
+        'month_id': month_id,
+    }
+    return render(request, 'statistics/view_outliers.html', context)
+
+
